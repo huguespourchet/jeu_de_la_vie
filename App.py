cad125324c1659426aa5840326f754ec2232eefe
hugues <huguespourchet@gmail.com>
2023-02-20 13:07:33 +0100

    chek nombre voisins avec bugs pour coos négatives

--- App.py
+++ App.py
@@ -2,14 +2,16 @@ import Tableau
 
 class App:
     #constantes
-    TAILLE = 10
+    COL = 15
+    LINE = 10
 
-    def main(taille):
+    def main(col, line):
         #création du tableau de base
-        tableau = Tableau.Tableau(taille)
+        tableau = Tableau.Tableau(col, line)
         tableau.fillTab()
         #affichage du résultat
         tableau.show()
+        tableau.checkNeighboors()
         
-    main(TAILLE)
+    main(COL, LINE)
         
--- Tableau.py
+++ Tableau.py
@@ -3,16 +3,64 @@ import random
 class Tableau:
 
     matrice = []
-    taille = 0
+    col = 0
+    line = 0
 
-    def __init__(self, taille):
-        self.matrice = [[0] * taille] * taille
-        self.taille = taille
+    def __init__(self, col, line):
+        for i in range(line):
+            ligne = []
+            for j in range(col):
+                ligne.append(0)
+            self.matrice.append(ligne)
+
+        self.col, self.line = col, line
     
     def fillTab(self):
-        for i in range(self.taille):
-            for j in range(self.taille):
+        for i in range(self.line):
+            for j in range(self.col):
                 self.matrice[i][j] = int(random.randint(0, 1))
     
     def show(self):
-        print(self.matrice)
+        for i in range(self.line):
+            for j in range(self.col):
+                if self.matrice[i][j] == 1:
+                    print("O ", end="")
+                else:
+                    print("  ", end="")
+            print()
+
+    def checkNeighboors(self):
+        mat = []
+        for i in range(self.line):
+            for j in range(self.col):
+                compteur = 0
+                try:
+                    if self.matrice[i][j-1] == 1:
+                        compteur +=1
+                    if self.matrice[i-1][j-1] == 1:
+                        compteur +=1
+                        print(2)
+                    if self.matrice[i-1][j] == 1:
+                        compteur +=1
+                        print(3)
+                    if self.matrice[i-1][j+1] == 1:
+                        compteur +=1
+                        print(3)
+                    if self.matrice[i][j+1] == 1:
+                        compteur +=1
+                        print(4)
+                    if self.matrice[i+1][j+1] == 1:
+                        compteur +=1
+                        print(5)
+                    if self.matrice[i+1][j] == 1:
+                        compteur +=1
+                        print(6)
+                    if self.matrice[i+1][j-1] == 1:
+                        compteur +=1
+                        print(7)
+                except:
+                    compteur = compteur
+                mat.append(compteur)
+                print(mat)
+                exit()
+        print(mat)   
